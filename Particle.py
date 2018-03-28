666eeccae09aaaa2f972da995f0eb8165396cf97
italo <italotabatinga@gmail.com>
2018-03-28 16:35:14 -0300

    Refactoring Particle.py to PyQt5 classes

--- Particle.py
+++ Particle.py
@@ -1,37 +1,47 @@
-from PyQt5.QtGui import QVector2D
+from PyQt5.QtGui import QVector2D, QPainter, QColor
+from PyQt5.QtWidgets import QGraphicsItem
+# from PyQt5.QtCore import QRectF
 
-class Particle:
+class Particle(QGraphicsItem):
     """Particle class to simulate physics"""
-    def __init__(self, pos:QVector2D, **kwargs):
+    def __init__(self, pos: QVector2D, **kwargs):
         """Constructor to create a particle, receives a QtVector2D and can \
-        receive 'r' that is radius (float) and 'vel' that is velocity (QVector2d)"""
-        self.pos = pos
+        receive 'r' that is radius (float) and 'speed' that is speed (QVector2d)"""
+        self.setPos(pos.x(), pos.y())
         self.rad = kwargs.get('r', 10)
-        self.vel = kwargs.get('vel', QVector2D(10,10))
+        self.speed = kwargs.get('speed', QVector2D(10, 10))
 
-    def hasCollidedX(self, width : float):
+    def hasCollidedX(self):
         """Return true if the particle has collided with the vertical \
         borders"""
-        if(self.pos.x() - self.rad < 0 or self.pos.x()+self.rad > width):
+        newpos = self.mapToScene(self.pos())
+        if(newpos.x() - self.rad < 0 or newpos.x()+self.rad > self.scene().width()):
             return True
         return False
 
-    def hasCollidedY(self, height : float):
+    def hasCollidedY(self):
         """Return true if the particle has collided with the horizontal \
         borders"""
-        if(self.pos.y() - self.rad < 0 or self.pos.y()+self.rad > height):
+        newpos = self.mapToScene(self.pos())
+        if(newpos.y() - self.rad < 0 or newpos.y()+self.rad > self.scene().height()):
             return True
         return False
 
-    def update(self):
+    def advance(self, step):
         """Update the motion of the particle"""
-        self.pos += self.vel
-        if(self.hasCollidedX()):
-            self.vel.setX(self.vel.x()*-1)
-        if(self.hasCollidedY()):
-            self.vel.setY(self.vel.y()*-1)
+        if not step:
+            return
+        self.setPos(self.pos + self.speed)
+        if self.hasCollidedX():
+            self.speed.setX(self.speed.x()*-1)
+        if self.hasCollidedY():
+            self.speed.setY(self.speed.y()*-1)
 
-
-    def show(self):
-        """Show particle on the screen"""
+    def boundingRect(self):
         pass
+        # return QRectF(-self.rad, -self.rad, self.rad*2, self.rad*2)
+
+    def paint(self, painter: QPainter):
+        """Show particle on the screen"""
+        painter.setBrush(QColor(255, 255, 255))
+        painter.drawEllipse(self.boundingRect())
